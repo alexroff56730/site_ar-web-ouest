146f11239a38443df35eb53ad097713b0ce40b3d
unknown <alexandreroffiaen@gmail.com>
2023-01-25 17:32:45 +0100

    ajout des metas descriptions

--- devis.php
+++ devis.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>AR-WEB-OUEST - Demande de devis</title>
+    <meta name="description" content="Développeur et concepteur de site web indépendant, demandez votre devis en ligne et créez votre site web">
     <link rel="stylesheet" href="style/style.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
 </head>
