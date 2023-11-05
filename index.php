2d934831586e7546a5b15d7c32fa81e2f743b634
unknown <alexandreroffiaen@gmail.com>
2023-11-05 01:03:40 +0100

    service web ajout de contenue

--- index.php
+++ index.php
@@ -2,6 +2,54 @@
     include("header.php");
 ?>
 
+<div class="parallax row">
+    <div class="container d-flex align-items-center">
+        <article class="container col-lg-8 boxcolor1">
+            <h1 class="text-light text-left titleboxcolor1">AR-WEB-OUEST Développeur web</h1>
+            <p class="text-primary text-uppercase text-center">
+                vous connaître pour
+            </p>
+            <p class="text-danger text-uppercase text-right">
+                vous faire connaître...
+            </p>
+        </article>
+    </div>
+</div>
+<div class="boxExp bg-dark">
+    <div class="container">
+        <h2 class="text-light text-center">Création de votre site web</h2>
+        <div class="d-sm-block pt-4 d-md-block d-lg-flex">
+            <article class="col">
+                <h2 class="text-light NStitle text-center">Rédaction du cahier des charges</h2>
+                <p class="text-light pt-2 text-justify">
+                    Analyse de votre projet dans son enssemble, 
+                    pour comprendre votre activité, définir une charte graphique sur mesure ainsi que les technologies
+                    à utiliser pour valoriser votre 
+                    <strong class="font-weight-normal">site</strong> sur le <strong class="font-weight-normal">web</strong>.
+                </p>
+            </article>
+            <article class="col">
+                <h2 class="text-light NStitle text-center">Validation et mise en production</h2>
+                <p class="text-light pt-2 text-justify">
+                    Une fois le <strong class="font-weight-normal">Développement</strong> effectué, 
+                    nous testons les diverses fonctionalité du site
+                    pour pouvoir l'héberger sur un serveur et éventuellement ajouter de nouvelles fonctionalitées
+                </p>
+            </article>
+            <article class="col">
+                <h2 class="text-light NStitle text-center">Maintenance et conseil</h2>
+                <p class="text-light pt-2 text-justify">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae, voluptas!
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque, fugiat?
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate, quidem.
+                    Lorem ipsum dolor sit
+                </p>
+            </article>
+            
+        </div>    
+    </div>
+</div>
+
 <?php
     include("footer.php");
 ?>
